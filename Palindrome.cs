6a6603fedb45e7a869e150bd2a4f41363c64b316
Muskan Garg <60514953+Muskan20Garg@users.noreply.github.com>
2020-03-12 18:44:13 +0530

    Add files via upload

---
+++ Palindrome.cs
@@ -0,0 +1,24 @@
+using System;  
+namespace abc
+{
+  public class Palindrome
+   {  
+     public static void Main(string[] args)  
+      {  
+          int n,r,sum=0,temp;    
+          Console.Write("Enter the Number: ");   
+          n = int.Parse(Console.ReadLine());  //parse() method is used to parse the string 
+          temp=n;      
+          while(n>0)      
+          {      
+           r=n%10;      
+           sum=(sum*10)+r;      
+           n=n/10;      
+          }      
+          if(temp==sum)      
+           Console.Write("Number is Palindrome.");      
+          else      
+           Console.Write("Number is not Palindrome");     
+    }  
+  }
+}  
